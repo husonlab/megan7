ec6e860d07b08c2e0fba72ca3231a6a0201d09c4
Daniel <daniel.huson@uni-tuebingen.de>
2025-06-26 08:02:28 +0200

    new version

--- src/main/java/megan/main/Version.java
+++ src/main/java/megan/main/Version.java
@@ -25,6 +25,6 @@ package megan.main;
  */
 public class Version {
 	static public String NAME = "MEGAN 7";
-	static public String SHORT_DESCRIPTION = "MEGAN 7 Community Edition (version 7.0.18-beta, built  4 Jun 2025)";
+	static public String SHORT_DESCRIPTION = "MEGAN 7 Community Edition (version 7.0.20-beta, built  5 Jun 2025)";
 }
 
